771b33f41de0cd03ef99bb88178e3d3f13dd7766
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-10 19:14:15 -0300

    @johnnymarcelino [Learning about static members done] {ouct.10 at 19:14h}

---
+++ Projetos_Iniciante/StaticMembers/StaticMembers/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticMembers {
+    internal class Calculator {
+
+        public static double Pi = 3.14;
+
+        public static double Circumference(double r) {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r) {
+            //return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0); // This method works too.
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
---
+++ Projetos_Iniciante/StaticMembers/StaticMembers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticMembers {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            Console.Write("Enter with the value of radious: ");
+            double radious = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculator.Circumference(radious);
+            double volum = Calculator.Volume(radious);
+
+            Console.WriteLine("Circumference: " + circ.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volum.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Value of PI: " + Calculator.Pi.ToString("f2", CultureInfo.InvariantCulture));
+        }
+    }
+}
