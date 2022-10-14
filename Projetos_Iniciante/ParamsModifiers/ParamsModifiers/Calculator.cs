1bc5fffaebe82d6a4195c7fd17ddafc545f26c27
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-14 09:21:39 -0300

    @johnnymarcelino [Learning about Parameters Modifiers added] {ouct.14 at 09:21h}

---
+++ Projetos_Iniciante/ParamsModifiers/ParamsModifiers/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ParamsModifiers {
+    class Calculator {
+
+        // Estes são exemplos de sobrecargas
+        /*
+        public static int Sum(int n1, int n2) {
+            return n1 + n2;
+        }
+        public static int Sum(int n1, int n2, int n3) {
+            return n1 + n2 + n3;
+        }
+        public static int Sum(int n1, int n2, int n3, int n4) {
+            return n1 + n2 + n3 + n4;
+        }
+        */
+
+        // Estes são exemplos mais sofisticados com uma funçao de vetores / listas
+
+        // Esta forma deixa um pouco redundante a função
+        /*
+        public static int Sum(int[] numbers) {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        */
+
+
+        // Estes são exemplos mais sofisticados com uma funçao de vetores / listas e parametro params
+        // já esta deixa o código mais sucinto com o modificador de parametros params
+
+        /*
+        public static int Sum(params int[] numbers) {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        */
+
+        // Modificador ref
+        /*
+        public static void Triple(int x) { // sem a utilização da palavra ref não podemos ter referencia
+            x = x * 3;
+            System.Console.WriteLine(x);
+        }
+        */
+
+        // com a palavra ref em uso guardamos tambem na variavel do program
+        /*
+        public static void Triple(ref int x) {
+            x = x * 3;
+            System.Console.WriteLine(x);
+        }
+        */
+
+
+        // modificador out
+        public static void Triple(int origin, out int result) {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Projetos_Iniciante/ParamsModifiers/ParamsModifiers/Program.cs
@@ -0,0 +1,39 @@
+﻿//using ParamsModifiers;
+using System;
+
+namespace ParamsModifiers {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            /*
+            int s1 = Calculator.Sum(new int[] { 10, 15 } );  // atribuindo valores aos vetores/lista
+            int s2 = Calculator.Sum(new int[] { s1 + 1, 15, 20 });  // exemplo -> new type [] {}
+            */
+
+            /*
+            int s3 = Calculator.Sum(10, 15 );  // utilização do modificador params
+            int s4 = Calculator.Sum(13 + 1, 15, 20);  // utilização do modificador de parametro params
+
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            */
+
+
+            /*
+            // modificador ref
+
+            int a = 10;
+            //Calculator.Triple(a); // sem a palavra ref n podemos chamar a função Triple
+            Calculator.Triple(ref a); // com a palavra ref, podemos chamar a função Triple
+            Console.WriteLine(a);
+            */
+
+
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
