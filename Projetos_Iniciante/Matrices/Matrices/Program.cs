d67415d34fae104a8ae82ea1bae70364ddf792d2
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-15 23:00:52 -0300

    @johnnymarcelino [Learning about Matrices added] {ouct.14 at 11:01pm}

---
+++ Projetos_Iniciante/Matrices/Matrices/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Matrices {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double[,] matrix = new double[4 , 5];
+
+            // tamanho da matriz
+            Console.WriteLine(matrix.Length);
+
+            // dimensão da matriz inicial
+            Console.WriteLine(matrix.Rank);
+
+            // retorna o valor referente ao tamanho da posição desejada no indice
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+        }
+    }
+}
