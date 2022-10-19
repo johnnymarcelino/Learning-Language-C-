cde871fd23da7d9111276df0f41aee8e592b3293
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-19 20:06:19 -0300

    @johnnymarcelino [Learning about VarType added] {ouct.19 at 20:06h}

---
+++ Projetos_Iniciante/Var_Swit_Tern/Var_Swit_Tern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Var_Swit_Tern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.00;
+            var z = "Maria";
+
+            // utilização de tipos de dados para ser informado pela idle
+            var w = z;
+            //int w = z;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
