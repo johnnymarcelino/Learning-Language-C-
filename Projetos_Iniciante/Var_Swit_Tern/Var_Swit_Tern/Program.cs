ba1304a7f969c481dfa12c5c4f7422f5b27626f8
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-20 23:45:42 -0300

    @johnnymarcelino [Learning about Switch-Case added] {ouct.20 at 23:46h}

--- Projetos_Iniciante/Var_Swit_Tern/Var_Swit_Tern/Program.cs
+++ Projetos_Iniciante/Var_Swit_Tern/Var_Swit_Tern/Program.cs
@@ -6,6 +6,9 @@ namespace Var_Swit_Tern
     {
         static void Main(string[] args)
         {
+            /*
+            // estudo da palavra VAR
+
             var x = 10;
             var y = 20.00;
             var z = "Maria";
@@ -17,6 +20,79 @@ namespace Var_Swit_Tern
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(z);
+            */
+
+
+            // Estudo do Switch-case
+
+            /*
+            This method is about if-else in chain
+
+            int wee = int.Parse(Console.ReadLine());
+            string day;
+
+            if (wee == 1) {
+                day = "Sunday";
+            }
+            else if (wee == 2) {
+                day = "Monday";
+            }
+            else if (wee == 3) {
+                day = "Tuesday";
+            }
+            else if (wee == 4) {
+                day = "Whenesday";
+            }
+            else if (wee == 5) {
+                day = "Thursday";
+            }
+            else if (wee == 6) {
+                day = "Friday";
+            }
+            else if (wee == 7) {
+                day = "Saturday";
+            }
+            else {
+                day = "Invalid value";
+            }
+
+            Console.WriteLine("Day: " + day);
+            */
+
+            // And this method is about switch-case
+
+            int wee = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (wee) {
+                case 1: 
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Whenesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value";
+                    break;
+            }
+
+            Console.WriteLine("Day: " + day);
+
         }
     }
 }
