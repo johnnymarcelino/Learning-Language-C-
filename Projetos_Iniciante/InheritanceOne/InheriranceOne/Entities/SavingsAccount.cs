27cb06d6c91a7d96a8ccc37fe8c97b94d9ccfdf4
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-26 16:30:03 -0300

    @johnnymarcelino [Learning about Overlap, virtual word, overrride and base updated with Sealed] {oct.26.2022 at 4:30pm}

--- Projetos_Iniciante/InheritanceOne/InheriranceOne/Entities/SavingsAccount.cs
+++ Projetos_Iniciante/InheritanceOne/InheriranceOne/Entities/SavingsAccount.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace InheriranceOne.Entities
 {
-    internal class SavingsAccount : Account
+    sealed class SavingsAccount : Account  // classe selada - limitada para utilizar em outras classes; comum internal ou apenas class
     {
         public double InterestRate { get; set; }
 
@@ -24,11 +24,23 @@ namespace InheriranceOne.Entities
 
         // Trabalhando com Sobreposição/Sobrescrito, palavra virtual, override e base - subclasses
 
-        public override void Withdraw(double amount)  // override usado nas subclasses
+        /*
+        public override void Withdraw(double amount)  // override usado nas subclasses - sobreposto (override)
         {
             //Balance -= amount;
             base.Withdraw(amount);  // invocar o método comum da superclasse
             Balance -= 2.0;
         }
+
+        */
+
+
+        public sealed override void Withdraw(double amount)  // override usado nas subclasses - sobreposto (override) e selada (sealed)
+        {
+            //Balance -= amount;
+            base.Withdraw(amount);  // invocar o método comum da superclasse
+            Balance -= 2.0;
+        }
+
     }
 }
