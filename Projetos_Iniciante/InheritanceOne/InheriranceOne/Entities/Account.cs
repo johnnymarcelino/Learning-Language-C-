2730c3c1d6bc1da42a96582a84a76d808aa8d6c8
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-10-26 09:33:03 -0300

    @johnnymarcelino [Learning about Inheritance added] {oct.26.2022 at 09:31am}

---
+++ Projetos_Iniciante/InheritanceOne/InheriranceOne/Entities/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheriranceOne.Entities
+{
+    internal class Account
+    {
+        public int Number { get; protected set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+    }
+}
---
+++ Projetos_Iniciante/InheritanceOne/InheriranceOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using InheriranceOne.Entities;
+using System;
+
+namespace InheriranceOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount();
+
+            Console.WriteLine(account.Balance);
+
+            // account.Balance = 400;
+        }
+    }
+}
