851754c89d3cdbce8f042f7b63afa11efb49f007
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-11-25 23:58:54 -0300

    @johnnymarcelino [Files and Filesinfo] {nov.25.2022 12:59pm}

---
+++ Projetos_Iniciante/Files/Files/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+
+// Trabalhando com Files
+
+
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // File
+
+            /*
+            
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+            try
+            {
+                File.Copy(sourcePath, targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            */
+
+            // FileInfo
+
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
