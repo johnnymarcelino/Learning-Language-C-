a0b352cd3b5df5a8ec99cfa9381a71784bdb7f26
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-02 09:44:08 -0300

    @johnnymarcelino [Learnig about Generics part 1 added] {dec.02.2022 9:44am}

---
+++ Projetos_Iniciante/Generics/Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printservice = new PrintService();
+
+            Console.WriteLine("How many values ?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printservice.AddValue(x);
+            }
+
+            printservice.Print();
+            Console.WriteLine();
+            Console.WriteLine("First: " + printservice.First());
+        }
+    }
+}
