b4fe747b778092596b8a8db355e50ceab3b1ce3c
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-02 12:31:10 -0300

    @johnnymarcelino [Learnig about Generics Restrictions - in process] {dec.02.2022 12:31pm}

---
+++ Projetos_Iniciante/GenericRestrictions/GenericRestrictions/Program.cs
@@ -0,0 +1,34 @@
+﻿using GenericRestrictions.Entities;
+using GenericRestrictions.Service;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace GenericRestrictions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.WriteLine("Enter N:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(",");
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+
+            CalculationsService calculationsService = new CalculationsService();
+
+            Product max = calculationsService.Produ(list);
+
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+        }
+    }
+}
