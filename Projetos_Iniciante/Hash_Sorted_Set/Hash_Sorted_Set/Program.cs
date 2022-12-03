8fdc8504a520e0a0d63eaf607a8bbc430df1cbd6
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-03 10:06:19 -0300

    @johnnymarcelino [Learnig about HashSet and SortedSet in process] {dec.03.2022 10:06am}

---
+++ Projetos_Iniciante/Hash_Sorted_Set/Hash_Sorted_Set/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hash_Sorted_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("tv"));
+
+            foreach (string s in set)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
