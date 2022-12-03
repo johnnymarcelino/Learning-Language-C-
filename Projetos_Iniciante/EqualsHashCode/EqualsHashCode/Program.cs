d2b6bc55def8aa24904914c2972698430be02e8b
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-03 09:51:12 -0300

    @johnnymarcelino [Learnig about GetHashCode and Equals done] {dec.03.2022 9:51am}

---
+++ Projetos_Iniciante/EqualsHashCode/EqualsHashCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using EqualsHashCode.Entities;
+using System;
+
+namespace EqualsHashCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@email.com" };
+
+            Client b = new Client { Name = "João", Email = "joao@email.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);  // comparação por referencia de memoria
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
