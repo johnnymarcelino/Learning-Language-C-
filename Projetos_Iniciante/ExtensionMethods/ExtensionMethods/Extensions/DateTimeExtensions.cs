0c2b38cc7485af1fedfe30b1f5526021c5e45567
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-08 21:33:11 -0300

    @johnnymarcelino [Learnig about Extensions Methods - Part 1] {dec.08.2022 9:33pm}

---
+++ Projetos_Iniciante/ExtensionMethods/ExtensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExtensionMethods.Extensions  // podemos simplesmente declarar o namespace da classe como do System, pois os tipos já pertence a ele
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)  // esse é um metodo de extensão do tipo datetime -> this primeiro e o tipo desejado
+        {
+            //TimeSpan duration = DateTime.Now - thisObj; //  há diversas maneiras de realizar a lógica desejada
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);  // o parametro this apresenta-se sendo como o próprio objeto passado na invocação
+
+            if (duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " Hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Days";
+            }
+        }
+    }
+}
---
+++ Projetos_Iniciante/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2022, 12, 8, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
