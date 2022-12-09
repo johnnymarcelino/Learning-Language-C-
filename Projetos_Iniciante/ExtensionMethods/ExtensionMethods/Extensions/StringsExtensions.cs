b8a61d20db59cd78b1b4d9bf863e1fbf190a4cb7
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-08 21:51:22 -0300

    @johnnymarcelino [Learnig about Extensions done] {dec.08.2022 9:52pm}

---
+++ Projetos_Iniciante/ExtensionMethods/ExtensionMethods/Extensions/StringsExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace System  // utilizando o macete do namespace que é proprietário do tipo utilizado
+{
+    static class StringsExtensions
+    {
+        public static string Cut(this string thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+            {
+                return thisObj;
+            }
+            else
+            {
+                return thisObj.Substring(0, count) + "...";
+            }
+        }
+    }
+}
--- Projetos_Iniciante/ExtensionMethods/ExtensionMethods/Program.cs
+++ Projetos_Iniciante/ExtensionMethods/ExtensionMethods/Program.cs
@@ -7,8 +7,19 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
+            // Part 1
+
+            /*
+            
             DateTime dt = new DateTime(2022, 12, 8, 8, 10, 45);
             Console.WriteLine(dt.ElapsedTime());
+
+            */
+
+            // Part 2
+
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
         }
     }
 }
