0e4a05a2a70be958492cb6d3b6dee553e5017271
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-09 23:55:46 -0300

    @johnnymarcelino [Learnig about Predicate done] {dec.09.2022 11:56pm}

---
+++ Projetos_Iniciante/Predicate/Predicate/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
+using Predicate.Entities;
+
+namespace Predicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //list.RemoveAll(p => p.Price >= 100.0);  // há duas maneiras de se fazer atraves de referencia e expressão lambda
+            list.RemoveAll(ProductTest);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+            /*
+            User user = new User()
+            {
+                Name = "johnny",
+                Email = "invalid email"
+            };
+
+            var context = new ValidationContext(user, null, null);
+            var results = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(user, context, results, true);
+
+            if (!isValid)
+            {
+                // validation failed, display errors
+                foreach (var result in results)
+                {
+                    Console.WriteLine(result.ErrorMessage);
+                }
+            }
+            else
+            {
+                // validation passed, display approved
+                foreach (var result in results)
+                {
+                    Console.WriteLine("passed");
+                }
+            }
+
+            */
+        }
+
+        public static bool ProductTest(Product product)
+        {
+            return product.Price >= 100.00;
+        }
+    }
+}
---
+++ Projetos_Iniciante/Predicate/Predicate/User.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
+//using System.Text;
+
+//namespace Predicate
+//{
+//    internal class User
+//    {
+//        [Required]
+//        public string Name { get; set; }
+
+//        [StringLength(100)]
+//        public string Email { get; set; }
+//    }
+//}
