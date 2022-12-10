e39cb27bed9c088f554daf022f44e912766641bf
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-12-10 10:34:29 -0300

    @johnnymarcelino [Learnig about Linq] {dec.10.2022 10:34am}

---
+++ Projetos_Iniciante/Linq/Linq/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            // Define the query
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // Define the query
+            // with lists 
+            List<int> result2 = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10).ToList();  // tolist cause IEnumerable don't convert to list
+
+            // Define the query
+            // with IEnumerable
+            IEnumerable<int> result3 = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10); // IEnumerable is generic in to a lista for multiple collections
+
+            // Execute the query
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
