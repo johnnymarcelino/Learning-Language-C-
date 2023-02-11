2321477443081bfa60694574f2929ac95f9d76d0
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-02-11 10:05:30 -0300

    feature: FullCsharpTutorialEU7Hours created

---
+++ Projetos_Iniciante/FullCsharpTutorialEU7Hours/FullCsharpTutorialEU7Hours/Program.cs
@@ -0,0 +1,14 @@
+﻿public class FullCsharp
+{
+    public static void Main()
+    {
+        // See https://aka.ms/new-console-template for more information
+        //int result = 2 + 2;
+        //Console.WriteLine(result);
+
+        float productA = 3.123f;
+        float productB = 3.124f;
+        Console.WriteLine(productA);
+        Console.WriteLine(productB);
+    }
+}
