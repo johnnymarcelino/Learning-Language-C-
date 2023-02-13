6a3cd386e3fb6ec047d656b89545ce6808842aef
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-02-12 23:47:21 -0300

    feature: remembering methods

--- Projetos_Iniciante/FullCsharpTutorialEU7Hours/FullCsharpTutorialEU7Hours/Program.cs
+++ Projetos_Iniciante/FullCsharpTutorialEU7Hours/FullCsharpTutorialEU7Hours/Program.cs
@@ -1,14 +1,45 @@
 ﻿public class FullCsharp
 {
+
+    // Explicitly
+
+    //float price = 3.99f;
+    //string name = "Jannick";
+    //char letter = 'a';
+    //int number = 10000;
+    //bool toggle = false;
+
     public static void Main()
     {
-        // See https://aka.ms/new-console-template for more information
-        //int result = 2 + 2;
-        //Console.WriteLine(result);
 
-        float productA = 3.123f;
-        float productB = 3.124f;
-        Console.WriteLine(productA);
-        Console.WriteLine(productB);
+        //float productA = 3.123f;
+        //float productB = 3.124f;
+        //Console.WriteLine(productA);
+        //Console.WriteLine(productB);
+
+        // Implicitly
+
+        //var username = 10000000000;
+        //Console.WriteLine(username);
+
+        WriteSomething();
+        WriteSomethingSpecific("I am a argument and called from a method");
+        Console.ReadKey();
+    }
+
+    public static void WriteSomething()
+    {
+        Console.WriteLine("I am called from the a method");
+    }
+
+    public static void WriteSomethingSpecific(string myText)
+    {
+        Console.WriteLine(myText);
+    }
+
+    public static int Add(int num1, int numb2)
+    {
+        int a = 12;
+        return a;
     }
 }
