6506ba1a14e7d09936e52045f74f67e155e19309
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-03-28 19:04:53 -0300

    feat: everything about unit tests - part 1

--- Projetos_Iniciante/UnitTests/UnitTests/Program.cs
+++ Projetos_Iniciante/UnitTests/UnitTests/Program.cs
@@ -1,12 +1,277 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.Intrinsics.X86;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace UnitTests
 {
     internal class Program
     {
+        // CONTEÚDOS IMPORTANTES SOBRE TESTES UNITÁRIO
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //1.Crie testes unitários para garantir que todos os componentes de cada aplicação estejam funcionando corretamente.
+            //2.Crie testes de integração para verificar se os vários componentes de uma aplicação estão trabalhando em conjunto
+            //de forma adequada.
+            //3.Crie testes de aceitação para verificar se os requisitos da aplicação estão sendo satisfeitos.
+            //4.Crie testes de estresse para verificar o comportamento da aplicação em situações de alta carga.
+            //5.Crie testes de desempenho para avaliar as métricas de desempenho da aplicação.
+            //6.Crie testes de segurança para garantir que a aplicação esteja protegida contra ataques de hackers.
+            //7.Crie testes de qualidade de código para garantir que o códigcontinue mantendo padrões altos de qualidade.
+            //8.Crie testes de usabilidade para avaliar o nível de usabilidade da aplicação.
+            //9.Crie testes de compatibilidade para verificar se a aplicação funciona corretamente em diferentes plataformas.
+            //10.Crie testes de regressão para garantir que as novas alterações não tenham impactado outras partes da aplicação.
+
         }
+
+
+        // EM LINGUAGEM C#
+
+
+
+        //Teste unitario
+        public class UnitTest
+        {
+            public void testAdd()
+            {
+                int x = 5;
+                int y = 6;
+                int expectedResult = 11;
+                int actualResult = x + y;
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        //Teste de integração
+        [Test]
+        public void testDatabaseConnection()
+        {
+            DatabaseConnection db = new DatabaseConnection();
+            Assert.IsNotNull(db.getConnection());
+        }
+
+
+        //Teste de aceitação
+        [Test]
+        public void testApplicationFunctionality()
+        {
+            Application app = new Application();
+            Assert.IsTrue(app.execute());
+        }
+
+
+        //Teste de estresse
+        [Test]
+        public void testHighLoad()
+        {
+            Application app = new Application();
+            for (int i = 0; i < 1000; i++)
+            {
+                Assert.IsTrue(app.execute());
+            }
+        }
+
+
+        //Teste de desempenho
+        [Test]
+        public void testResponseTime()
+        {
+            Application app = new Applicationcontinue();
+            long startTime = System.DateTime.Now.Ticks;
+            app.execute();
+            long endTime = System.DateTime.Now.Ticks;
+            long responseTime = endTime - startTime;
+            Assert.IsTrue(responseTime < 1000);
+        }
+
+
+        //Teste de segurança
+        [Test]
+        public void testForSQLInjection()
+        {
+            Application app = new Application();
+            String maliciousInput = "'; DROP TABLE users; --";
+            Assert.IsFalse(app.continueexecute(maliciousInput));
+        }
+
+
+        //Teste de qualidade de código
+        [Test]
+        public void testCodeCoverage()
+        {
+            Code code = new Code();
+            Assert.IsTrue(code.getCoverage() >= 80);
+        }
+
+
+        //Teste de usabilidade
+        [Test]
+        public void testUserExperience()
+        {
+            Application app = new Application();
+            UserExperienceReport report = app.evaluateUserExperience();
+            Assert.IsTrue(report.getSatisfactionLevel() > 8);
+        }
+
+
+        //Teste de compatibilidade
+        [Test]
+        public void testCrossBrowserCompatibility()
+        {
+            Application app = new Application();
+            foreach (Browser browser in Enum.GetValues(typeof(Browser)))
+            {
+                Assert.IsTrue(app.execute(browser));
+            }
+        }
+
+
+        //Teste de regressão
+        [Test]
+        public void testNewChanges()
+        {
+            Application app = new Application();
+            app.execute();
+            //New changes
+            app.execute();
+            Assert.IsTrue(app.isWorking()); continue
+        }
+
+
+        // -------------------------------------------------------------- //
+
+
+        // EM LINGUAGEM JAVA - EXEMPLOS SEMELHANTES
+
+
+        ////Teste unitario
+        //public class UnitTest
+        //{
+        //    public void testAdd()
+        //    {
+        //        int x = 5;
+        //        int y = 6;
+        //        int expectedResult = 11;
+        //        int actualResult = x + y;
+        //        assertEquals(expectedResult, actualResult);
+        //    }
+        //}
+
+        ////Teste de integração
+        //public class IntegrationTest
+        //{
+        //    @Test
+        //    public void testDatabaseConnection()
+        //    {
+        //        DatabaseConnection db = new DatabaseConnection();
+        //        assertNotNull(db.getConnection());
+        //    }
+        //}
+
+        ////Teste de aceitação
+        //public class AcceptanceTest
+        //{
+        //    @Test
+        //    public void testApplicationFunctionality()
+        //    {
+        //        Application app = new Application();
+        //        assertTrue(app.execute());
+        //    }
+        //}
+
+        ////Teste de estresse
+        //public class StressTest
+        //{
+        //    @Test
+        //    public void testHighLoad()
+        //    {
+        //        Application app = new Application();
+        //        for (int i = 0; i < 1000; i++)
+        //        {
+        //            assertTrue(app.execute());
+        //        }
+        //    }
+        //}
+
+        ////Teste de desempenhocontinue
+        //public class PerformanceTest
+        //{
+        //    @Test
+        //    public void testResponseTime()
+        //    {
+        //        Application app = new Application();
+        //        long startTime = System.currentTimeMillis();
+        //        app.execute();
+        //        long endTime = System.currentTimeMillis();
+        //        long responseTime = endTime - startTime;
+        //        assertTrue(responseTime < 1000);
+        //    }
+        //}
+
+        ////Teste de segurança
+        //public class SecurityTest
+        //{
+        //    @Test
+        //    public void testForSQLInjection()
+        //    {
+        //        Application app = new Application();
+        //        String maliciousInput = "'; DROP TABLE users; --";
+        //        assertFalse(app.execute(maliciousInput));
+        //    }
+        //}
+
+        ////Teste de qualidade de código
+        //public class CodeQualityTest
+        //{
+        //    @Test
+        //    public void testCodeCoverage()
+        //    {
+        //        Code code = new Code();
+        //        assertTrue(code.getCoverage() >= 80);
+        //    }
+        //}
+
+        ////Teste de usabilidade
+        //public class UsabilityTest
+        //{
+        //    @Test
+        //    public void testUserExperience()
+        //    {
+        //        Application app = newcontinue Application();
+        //        UserExperienceReport report = app.evaluateUserExperience();
+        //        assertTrue(report.getSatisfactionLevel() > 8);
+        //    }
+        //}
+
+        ////Teste de compatibilidade
+        //public class CompatibilityTest
+        //{
+        //    @Test
+        //    public void testCrossBrowserCompatibility()
+        //    {
+        //        Application app = new Application();
+        //        for (Browser browser : Browser.values())
+        //        {
+        //            assertTrue(app.execute(browser));
+        //        }
+        //    }
+        //}
+
+        ////Teste de regressão
+        //public class RegressionTest
+        //{
+        //    @Test
+        //    public void testNewChanges()
+        //    {
+        //        Application app = new Application();
+        //        app.execute();
+        //        //New changes
+        //        app.execute();
+        //        assertTrue(app.isWorking());
+        //    }
+        //}
+
     }
 }
