4a3bb8a6d1356d5816d1ae1a4ecd6956f814aa1a
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-04-13 15:33:00 -0300

    feat: Working with LINQs

--- Projetos_Iniciante/AllAboutC#/AllAboutCsharp/AllAboutCsharp/Program.cs
+++ Projetos_Iniciante/AllAboutC#/AllAboutCsharp/AllAboutCsharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 
 //Neste exemplo, a variável "x" é do tipo "int", que representa um número inteiro,
@@ -30,7 +32,7 @@ namespace ExemploTipagemEGerenciamento
 
 namespace ExemploCompilacao
 {
-    class Program
+    class Program01
     {
         static void Main(string[] args)
         {
@@ -47,3 +49,137 @@ namespace ExemploCompilacao
 //pelo interpretador da linguagem, que executa o código em tempo de execução.
 
 //print("Olá, mundo!")
+
+// ------------------------------------------------------------------------ //
+
+/*
+    Trabalhando com LINQs 
+*/
+
+namespace LINQDemo
+{
+    class Program02
+    {
+        static void Main(string[] args)
+        {
+            //Data Source
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            //LINQ Query using Mixed Syntax
+            var MethodSyntax = (from obj in integerList
+                                where obj > 5
+                                select obj).Sum();
+
+            //Execution
+            Console.Write("Sum Is : " + MethodSyntax);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+namespace LINQDemo
+{
+    class Program03
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            IEnumerable<int> QuerySyntax = from obj in integerList
+                                           where obj > 5
+                                           select obj;
+
+            foreach (var item in QuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+namespace LINQDemo
+{
+    class Program04
+    {
+        static void Main(string[] args)
+        {
+            List<Student01> studentList = new List<Student01>()
+            {
+                new Student01(){ID = 1, Name = "James", Gender = "Male"},
+                new Student01(){ID = 2, Name = "Sara", Gender = "Female"},
+                new Student01(){ID = 3, Name = "Steve", Gender = "Male"},
+                new Student01(){ID = 4, Name = "Pam", Gender = "Female"}
+            };
+
+            //Linq Query to Fetch all students with Gender Male
+            IEnumerable<Student01> QuerySyntax = from std in studentList
+                                               where std.Gender == "Male"
+                                               select std;
+            //Iterate through the collection
+            foreach (var student in QuerySyntax)
+            {
+                Console.WriteLine($"ID : {student.ID}  Name : {student.Name}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Student01
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
+
+
+
+namespace LINQDemo
+{
+    class Program05
+    {
+        static void Main(string[] args)
+        {
+            List<Student02> studentList = new List<Student02>()
+            {
+                new Student02(){ID = 1, Name = "James", Gender = "Male"},
+                new Student02(){ID = 2, Name = "Sara", Gender = "Female"},
+                new Student02(){ID = 3, Name = "Steve", Gender = "Male"},
+                new Student02(){ID = 4, Name = "Pam", Gender = "Female"}
+            };
+
+            //Linq Query to Fetch all students with Gender Male
+            IQueryable<Student02> MethodSyntax = studentList.AsQueryable()
+                                .Where(std => std.Gender == "Male");
+
+            //Iterate through the collection
+            foreach (var student in MethodSyntax)
+            {
+                Console.WriteLine($"ID : {student.ID}  Name : {student.Name}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Student02
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
